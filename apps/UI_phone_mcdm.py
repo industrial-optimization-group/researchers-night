5674b027242a00aa22791f88a1bf5c5502f300c2
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-05-06 15:53:58 +0300

    Node selection

--- apps/UI_phone_mcdm.py
+++ apps/UI_phone_mcdm.py
@@ -1,39 +1,26 @@
 import dash
-from dash.exceptions import PreventUpdate
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
-import dash_table
-import plotly.express as ex
-import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv("./data/Phone_dataset_new.csv", header=0)
-details = pd.read_csv("./data/Phone_details.csv", header=0)
+data = pd.read_csv("./data/node_data.csv")
+print(data.head())
 
-names = details.loc[0]
+names = data.columns
 
-data = data.rename(columns=names)
-details = details.rename(columns=names)
-
+"""
 maxi = details.loc[1].astype(int)
 details_on_card = details.loc[2].astype(int)
 
 details_on_card = details.columns[details_on_card == 1]
-
-
-fitness_columns = {
-    "Memory": -1,
-    "RAM": -1,
-    "Camera (MP)": -1,
-    "Price (Euros)": 1,
-}
-
-fitness_data = data[fitness_columns] * maxi[fitness_columns].values
+"""
+maxi = [1, 1, 1]
+details_on_card = ["Cover", "Efficiency", "Activation", "Node combo"]
 
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -53,7 +40,7 @@ app.layout = html.Div(
             [
                 dbc.Col(
                     html.H1(
-                        children="What is your optimal phone?",
+                        children="Selecting a combination of nodes",
                         className="text-center mt-4",
                     )
                 )
@@ -63,79 +50,28 @@ app.layout = html.Div(
             [
                 dbc.Col(
                     children=[
-                        # Top card with details(?)
-                        dbc.Card(
-                            children=[
-                                dbc.CardBody(
-                                    [
-                                        html.H4(
-                                            "Researcher's Night Event",
-                                            className="card-title text-center",
-                                        ),
-                                        html.P(
-                                            (
-                                                "This app uses decision support tools to "
-                                                "quickly and easily find phones which reflect "
-                                                "the user's desires. Input your preferences "
-                                                "below. The box on top right shows the phone "
-                                                "which matches the preferences the best. "
-                                                "The box on bottom right provides some "
-                                                "close alternatives."
-                                            ),
-                                            className="card-text",
-                                        ),
-                                    ]
-                                )
-                            ],
-                            className="mr-3 ml-3 mb-2 mt-2",
-                        ),
                         dbc.Form(
                             [
                                 dbc.FormGroup(
                                     children=[
                                         dbc.Label(
-                                            "Choose desired operating system",
-                                            html_for="os-choice",
-                                        ),
-                                        dbc.RadioItems(
-                                            options=[
-                                                {
-                                                    "label": "Android",
-                                                    "value": "Android",
-                                                },
-                                                {"label": "iOS", "value": "IOS"},
-                                                {
-                                                    "label": "No preference",
-                                                    "value": "both",
-                                                },
-                                            ],
-                                            id="os-choice",
-                                            value="both",
-                                            inline=True,
-                                            # className="text-center mt-4",
-                                        ),
-                                    ],
-                                    className="mr-3 ml-3 mb-2 mt-2",
-                                ),
-                                dbc.FormGroup(
-                                    children=[
-                                        dbc.Label(
-                                            "Choose desired Memory capacity (GB)",
-                                            html_for="memory-choice",
+                                            "Cover",
+                                            html_for="cover",
                                         ),
                                         dcc.Slider(
-                                            id="memory-choice",
-                                            min=16,
-                                            max=256,
+                                            id="cover",
+                                            min=7,
+                                            max=12,
                                             step=None,
                                             included=False,
-                                            value=256,
+                                            value=7,
                                             marks={
-                                                16: "16",
-                                                32: "32",
-                                                64: "64",
-                                                128: "128",
-                                                256: "256",
+                                                7: "7",
+                                                8: "8",
+                                                9: "9",
+                                                10: "10",
+                                                11: "11",
+                                                12: "12",
                                             },
                                             # className="text-center mt-5",
                                         ),
@@ -145,28 +81,24 @@ app.layout = html.Div(
                                 dbc.FormGroup(
                                     children=[
                                         dbc.Label(
-                                            "Choose desired RAM capacity (GB)",
-                                            html_for="ram-choice",
+                                            "Efficiency",
+                                            html_for="efficiency",
                                         ),
                                         dcc.Slider(
-                                            id="ram-choice",
-                                            min=2,
-                                            max=12,
+                                            id="efficiency",
+                                            min=8,
+                                            max=14,
                                             step=1,
-                                            value=12,
+                                            value=14,
                                             included=False,
                                             marks={
-                                                2: "2",
-                                                3: "3",
-                                                4: "4",
-                                                5: "5",
-                                                6: "6",
-                                                7: "7",
                                                 8: "8",
                                                 9: "9",
                                                 10: "10",
                                                 11: "11",
                                                 12: "12",
+                                                13: "13",
+                                                14: "14",
                                             },
                                             className="text-center mt-5",
                                         ),
@@ -176,53 +108,25 @@ app.layout = html.Div(
                                 dbc.FormGroup(
                                     children=[
                                         dbc.Label(
-                                            "Choose desired camera resolution (MP)",
-                                            html_for="cam-choice",
+                                            "Activation",
+                                            html_for="activation",
                                         ),
                                         dcc.Slider(
-                                            id="cam-choice",
-                                            min=0,
-                                            max=130,
+                                            id="activation",
+                                            min=8,
+                                            max=15,
                                             step=1,
                                             included=False,
-                                            value=70,
+                                            value=15,
                                             marks={
-                                                0: "0",
+                                                8: "8",
+                                                9: "9",
                                                 10: "10",
-                                                30: "30",
-                                                50: "50",
-                                                70: "70",
-                                                90: "90",
-                                                110: "110",
-                                                130: "130",
-                                            },
-                                            className="text-center mt-5",
-                                        ),
-                                    ],
-                                    className="mr-3 ml-3 mb-2 mt-2",
-                                ),
-                                dbc.FormGroup(
-                                    children=[
-                                        dbc.Label(
-                                            "Choose desired budget (Euros)",
-                                            html_for="cost-choice",
-                                        ),
-                                        dcc.Slider(
-                                            id="cost-choice",
-                                            min=0,
-                                            max=1400,
-                                            step=1,
-                                            included=False,
-                                            value=100,
-                                            marks={
-                                                0: "0",
-                                                200: "200",
-                                                400: "400",
-                                                600: "600",
-                                                800: "800",
-                                                1000: "1000",
-                                                1200: "1200",
-                                                1400: "1400",
+                                                11: "11",
+                                                12: "12",
+                                                13: "13",
+                                                14: "14",
+                                                15: "15",
                                             },
                                             className="text-center mt-5",
                                         ),
@@ -239,14 +143,16 @@ app.layout = html.Div(
                     children=[
                         dbc.Card(
                             children=[
-                                dbc.CardHeader("The best phone for you is:"),
+                                dbc.CardHeader(
+                                    "The best combination of nodes with the given preferences:"
+                                ),
                                 dbc.CardBody(id="results"),
                             ],
                             className="mb-4",
                         ),
                         dbc.Card(
                             children=[
-                                dbc.CardHeader("Other great phones:"),
+                                dbc.CardHeader("Other closely matching combinations:"),
                                 dbc.CardBody(
                                     id="other-results",
                                     children=(
@@ -297,31 +203,25 @@ app.layout = html.Div(
         *[Output(f"other-results-list-{i}", "children") for i in range(2, 6)],
         *[Output(f"other-results-tooltip-{i}", "children") for i in range(2, 6)],
     ],
-    [
-        Input(f"{attr}-choice", "value")
-        for attr in ["os", "memory", "ram", "cam", "cost"]
-    ],
+    [Input(f"{attr}", "value") for attr in ["cover", "efficiency", "activation"]],
 )
 def results(*choices):
-    if choices[0] == "both":
-        choice_data = data
-    elif choices[0] == "IOS":
-        choice_data = data[[True if "IOS" in st else False for st in data["OS"]]]
-    if choices[0] == "Android":
-        choice_data = data[[True if "Android" in st else False for st in data["OS"]]]
-    relevant_data = choice_data[
-        ["Memory", "RAM", "Camera (MP)", "Price (Euros)",]
-    ].reset_index(drop=True)
+    choice_data = data
+    relevant_data = choice_data[["Cover", "Efficiency", "Activation"]].reset_index(
+        drop=True
+    )
     card_data = choice_data[details_on_card].reset_index(drop=True)
-    maxi = np.asarray([-1, -1, -1, 1])
+    maxi = np.asarray([1, 1, 1])
     relevant_data = relevant_data * maxi
     ideal = relevant_data.min().values
     nadir = relevant_data.max().values
-    aspirations = choices[1:] * maxi
+    aspirations = choices * maxi
     distance = (aspirations - relevant_data) / (ideal - nadir)
     distance = distance.max(axis=1)
     distance_order = np.argsort(distance)
-    best = table_from_data(card_data.loc[distance_order.values[0]], choices[1:])
+    tmp = card_data.loc[distance_order.values[0]]
+    print(f"{tmp=}")
+    best = table_from_data(card_data.loc[distance_order.values[0]], choices)
     total_number = len(distance_order)
     if total_number >= 4:
         others, tooltips = other_options(card_data.loc[distance_order.values[1:5]])
@@ -334,21 +234,12 @@ def results(*choices):
     return (best, *others, *tooltips)
 
 
-"""@app.callback(Output("tooltips", "children"), [Input("callback-dump", "children")])
-def tooltips(tooldict):
-    num = len(tooldict["ids"])
-    content = []
-    for i in range(num):
-        content.append(dbc.Tooltip(tooldict["tables"][i], target=tooldict["ids"][i]))
-    return content"""
-
-
 def table_from_data(data, choices):
     # print(choices)
-    to_compare = ["Memory", "RAM", "Camera (MP)", "Price (Euros)"]
+    to_compare = ["Cover", "Efficiency", "Activation"]
     # print(data[to_compare].values)
-    diff = (data[to_compare].values - choices) * [1, 1, 1, -1]
-    colors = [None, None, None] + ["green" if x >= 0 else "red" for x in diff]
+    diff = (data[to_compare].values - choices) * [1, 1, 1]
+    colors = ["green" if x >= 0 else "red" for x in diff]
     # print(np.sign(diff))
     return dbc.Table(
         [
@@ -357,11 +248,25 @@ def table_from_data(data, choices):
                     html.Tr(
                         [
                             html.Th(col),
-                            html.Td([str(data[col]),],),
-                            html.Td([html.Span(" ▉", style={"color": c,},)],),
+                            html.Td(
+                                [
+                                    str(data[col]),
+                                ],
+                            ),
+                            html.Td(
+                                [
+                                    html.Span(
+                                        " ▉",
+                                        style={
+                                            "color": c,
+                                        },
+                                    )
+                                ],
+                            ),
                         ]
                     )
                     for (col, c) in zip(data.index, colors)
+                    if col != "Node combo"
                 ]
             )
         ]
@@ -380,11 +285,11 @@ def other_options(data):
     ids = []
     i = 2
     for index, row in data.iterrows():
-        contents.append(f"{i}. {row['Model']}")
+        contents.append(f"{i}. {row['Node combo']}")
         tables.append(table_from_data_horizontal(row))
         i = i + 1
     return contents, tables
 
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
